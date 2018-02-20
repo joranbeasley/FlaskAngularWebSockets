ff073f255e2459147eabb9c3d0248699c80c0619
joran <joran@decagon.com>
2018-02-19 16:07:27 -0800

    updated package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FlaskInterviewPadApp',
+    version='0.0.1',
+    packages=['FlaskCodingPadApp','FlaskCodingPadApp.coderpad_socket_server'],
+    url='',
+    license='',
+    author='Joran Beasley',
+    author_email='',
+    description=''
+)
